2ece15f066458ff3f2bc5a9b1cc53851bd1cafe6
Stephan Abrimian <abrimians@gmail.com>
2020-01-26 16:51:22 -0500

    space invaders game using pygame

---
+++ SpaceInvaders.py
@@ -0,0 +1,21 @@
+import pygame
+
+pygame.init() #initilizes pygame
+
+#create the screen
+# screen = pygame.display.set_mode((800, 600))#need tuple in between parenthesis or it wont work
+screen = pygame.display
+screen.set_mode((800, 600))
+screen.set_caption(('Space Invaders: By yours truly')) #title
+
+
+
+
+
+#main loop(creates infinte loop)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
